bb8ff67223f09acc85dff545fc24417330ca8fad
hubqin <wuhanqinb@gmail.com>
2018-10-27 04:14:31 +0000

    finish user auth

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -17,8 +17,28 @@
 
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="{{ route('login') }}">登录</a></li>
-                <li><a href="{{ route('register') }}">注册</a></li>
+                @guest
+                    <li><a href="{{ route('login') }}">登录</a></li>
+                    <li><a href="{{ route('register') }}">注册</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" arial-expanded="false">
+                            <span class="user-avatar pull-left" style="margin-right: 8px; margin-top: -5px;">
+                                <img src="https://avatars3.githubusercontent.com/u/25000531?s=460&v=4" class="imgresponsive img-circle" width="30px" height="30px">
+                            </span>
+                            {{ Auth::user()->name }}<span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="post" style="display: none;">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
             </ul>
         </div>
     </div>
