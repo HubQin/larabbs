1ec59b5dc44e90550ab3d74504c79f444ebbcb79
HubQin <wuhanqinb@gmail.com>
2018-11-14 07:32:19 +0000

    add reply feature

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -6,48 +6,23 @@ use App\Models\Reply;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ReplyRequest;
+use Auth;
 
 class RepliesController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['index', 'show']]);
+        $this->middleware('auth');
     }
 
-	public function index()
+	public function store(ReplyRequest $request, Reply $reply)
 	{
-		$replies = Reply::paginate();
-		return view('replies.index', compact('replies'));
-	}
+		$reply->content = $request->content;
+        $reply->user_id = Auth::id();
+        $reply->topic_id = $request->topic_id;
+        $reply->save();
 
-    public function show(Reply $reply)
-    {
-        return view('replies.show', compact('reply'));
-    }
-
-	public function create(Reply $reply)
-	{
-		return view('replies.create_and_edit', compact('reply'));
-	}
-
-	public function store(ReplyRequest $request)
-	{
-		$reply = Reply::create($request->all());
-		return redirect()->route('replies.show', $reply->id)->with('message', 'Created successfully.');
-	}
-
-	public function edit(Reply $reply)
-	{
-        $this->authorize('update', $reply);
-		return view('replies.create_and_edit', compact('reply'));
-	}
-
-	public function update(ReplyRequest $request, Reply $reply)
-	{
-		$this->authorize('update', $reply);
-		$reply->update($request->all());
-
-		return redirect()->route('replies.show', $reply->id)->with('message', 'Updated successfully.');
+        return redirect()->to($reply->topic->link())->with('success', '创建成功');
 	}
 
 	public function destroy(Reply $reply)
@@ -55,6 +30,6 @@ class RepliesController extends Controller
 		$this->authorize('destroy', $reply);
 		$reply->delete();
 
-		return redirect()->route('replies.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('replies.index')->with('message', '删除成功！');
 	}
 }
