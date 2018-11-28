2edd25ee0eeb020469ec764340810e0af3f06305
HubQin <wuhanqinb@gmail.com>
2018-11-28 07:10:20 +0000

    User default avatar after signup

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -9,13 +9,10 @@ use App\Models\User;
 
 class UserObserver
 {
-    public function creating(User $user)
+    public function saving(User $user)
     {
-        //
-    }
-
-    public function updating(User $user)
-    {
-        //
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
     }
 }
