081d248c1394a35f0f745346bedb2fb674410249
hubqin <wuhanqinb@gmail.com>
2018-11-29 13:41:18 +0000

    close autocomplete of captcha input box

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -65,7 +65,7 @@
                             <label for="captcha" class="col-md-4 control-label">验证码</label>
 
                             <div class="col-md-6">
-                                <input id="captcha" class="form-control" name="captcha" >
+                                <input id="captcha" class="form-control" name="captcha" autocomplete="off">
 
                                 <img class="thumbnail captcha" src="{{ captcha_src('flat') }}" onclick="this.src='/captcha/flat?'+Math.random()" title="点击图片重新获取验证码">
 
